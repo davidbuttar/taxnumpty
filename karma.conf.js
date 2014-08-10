1535a02a641cbb161c550be8d5a42556ed0714aa
davidbuttar <david.buttar@gmail.com>
2014-08-10 20:30:13 +0100

    Stopping karma warnings

--- karma.conf.js
+++ karma.conf.js
@@ -13,15 +13,10 @@ module.exports = function(config) {
     files: [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-resource/angular-resource.js',
-      'app/bower_components/angular-cookies/angular-cookies.js',
-      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/jquery/jquery.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
